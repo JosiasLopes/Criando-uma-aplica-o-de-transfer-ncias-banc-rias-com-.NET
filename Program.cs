6b28a7e0239a5cbb83b3d9cbe12c4278592ec597
JosiasLopes <jobercardinho2014@gmail.com>
2021-04-09 17:07:49 -0300

    Primeiro commit criada a base da aplicação

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace console_banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var key = Console.ReadLine();
+            Console.WriteLine(key);
+        }
+    }
+}
